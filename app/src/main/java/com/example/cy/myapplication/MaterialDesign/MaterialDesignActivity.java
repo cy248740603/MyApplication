39278f0fc749cca65faca9047dcdd955d947dc14
CY <248740603@qq.com>
2017-08-22 13:58:27 +0800

    FloatingActionButton悬浮按钮 添加

--- app/src/main/java/com/example/cy/myapplication/MaterialDesign/MaterialDesignActivity.java
+++ app/src/main/java/com/example/cy/myapplication/MaterialDesign/MaterialDesignActivity.java
@@ -1,6 +1,7 @@
 package com.example.cy.myapplication.MaterialDesign;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.cy.myapplication.R;
 
@@ -34,6 +37,14 @@ public class MaterialDesignActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MaterialDesignActivity.this,"Fab clicked",Toast.
+                        LENGTH_SHORT).show();
             }
         });
     }
