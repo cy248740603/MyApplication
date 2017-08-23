d8d3d5cf3a0faabdd672510e3abbd87ce10bba91
CY <248740603@qq.com>
2017-08-23 09:48:53 +0800

    可折叠式标题栏 CollapsingToolbarLayout

--- app/src/main/java/com/example/cy/myapplication/MaterialDesign/FruitAdapter.java
+++ app/src/main/java/com/example/cy/myapplication/MaterialDesign/FruitAdapter.java
@@ -51,9 +51,20 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,
                 parent,false);
-
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext,FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME,fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID,fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
         //ViewHolder holder = new ViewHolder(view);
-        return new ViewHolder(view);
+        return holder;
     }
     public void onBindViewHolder(ViewHolder holder,int position){
         Fruit fruit = mFruitList.get(position);
