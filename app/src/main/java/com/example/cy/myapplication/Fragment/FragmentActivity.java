594e64de853bc5f711a6e50a434ad21e9de13795
CY <248740603@qq.com>
2017-08-24 10:29:56 +0800

    使用限定符 添加横屏 布局

--- app/src/main/java/com/example/cy/myapplication/Fragment/FragmentActivity.java
+++ app/src/main/java/com/example/cy/myapplication/Fragment/FragmentActivity.java
@@ -18,25 +18,25 @@ public class FragmentActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_fragment);
         Button button = (Button)findViewById(R.id.button_fragment);
         button.setOnClickListener(this);
-        replaceFragment(new RightFragment());
+       // replaceFragment(new RightFragment());
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.button_fragment:
-                replaceFragment(new Right1Fragment());
+                //replaceFragment(new Right1Fragment());
                 break;
             default:
                 break;
         }
     }
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.right_layout,fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
+//    private void replaceFragment(Fragment fragment){
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.right_layout,fragment);
+//        transaction.addToBackStack(null);
+//        transaction.commit();
+//    }
 
 }
